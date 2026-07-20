984ca69bc91893a11f01f827f35ead69bf17366a
Garv978 <tayalgarv777@gmail.com>
2026-07-20 21:05:09 +0530

    project setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import { useState } from 'react'
+import viteLogo from './assets/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     </>
+  )
+}
+
+export default App
